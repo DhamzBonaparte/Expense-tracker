3da0ca57266e27aff37b884ce1216e6a8001e17e
DhamzBonaparte <sulavdhami420@gmail.com>
2025-10-22 11:06:38 +0545

    changed the info stylesheet

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import "./info.scss";
+import "./Info.scss";
 import { mainContext } from "../Context/Context";
 
 export default function Info() {
